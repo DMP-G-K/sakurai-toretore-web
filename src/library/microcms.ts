32e27da73beba5f1e98226ccc05c24a6d9e3cc96
kivianko <kivianko@gmail.com>
2023-05-04 19:51:58 +0900

    Feat: microCMSとAPI連携

---
+++ src/library/microcms.ts
@@ -0,0 +1,32 @@
+//SDK利用準備
+import { createClient, MicroCMSImage, MicroCMSListResponse, MicroCMSQueries } from "microcms-js-sdk";
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+//型定義
+export type News = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  eyecatch: MicroCMSImage;
+  content: string;
+  category: string[];
+};
+
+//APIの呼び出し
+export const getNews = async (queries?: MicroCMSQueries) => {
+  return await client.get<MicroCMSListResponse<News>>({ endpoint: "news", queries });
+};
+
+export const getNewsDetail = async (contentId: string, queries?: MicroCMSQueries) => {
+  return await client.getListDetail<News>({
+    endpoint: "news",
+    contentId,
+    queries,
+  });
+};
