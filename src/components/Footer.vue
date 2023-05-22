ef961b30715eb50ffc0ffd3fd7a78ceb16169e56
y-kasuno <cubeee1104@gmail.com>
2023-05-22 19:38:45 +0900

    feat: フッターコンポーネントを作成
    # このPRの対応範囲
    フッターのHTMLのみ作成完了
    
    close #2

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <div>
+          <a href="#"><img src="public/svgs/logo.svg" alt="logo" /></a>
+        </div>
+        <div>
+          <p>さくらいとれとれ市場</p>
+          <p>Sakurai Toretore Market</p>
+        </div>
+      </div>
+      <div>
+        <p>〒633-0062<br />奈良県桜井市大字粟殿72番地<br />Google map →</p>
+      </div>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <a href="#">店舗情報<br /></a>
+        </li>
+        <li>
+          <a href="#">取り扱い商品<br /></a>
+        </li>
+        <li>
+          <a href="#">お知らせ<br /></a>
+        </li>
+        <li>
+          <a href="#">出荷者募集<br /></a>
+        </li>
+        <li>
+          <a href="#">お問い合わせ<br /></a>
+        </li>
+      </ul>
+    </div>
+    <p>Copyright © 2018-2023 さくらいとれとれ市場 All rights reserved.</p>
+  </div>
+</template>
