7a4eedd99fc36af53947090112ce30ad2d8e2865
kivianko <kivianko@gmail.com>
2023-06-04 17:32:22 +0900

    feat: ハンバーガーメニューのコンポーネントを作成
    ## 対応範囲
    - ヘッダーのレイアウトを修正
    - ドロワーメニューコンポーネントの追加
    - Vueトランジションアニメーションの追加
    - ヘッダーをVueファイルに変更
    
    close #63

---
+++ src/components/DrawerMenu.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="wrapper">
+    <ul class="nav">
+      <span class="shape-upper-left"></span>
+      <span class="shape-bottom-right"></span>
+      <li class="nav-text">
+        <a href="#"
+          >店舗情報
+          <p class="nav-text-sub">INFOMATION</p>
+        </a>
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >取り扱い商品
+          <p class="nav-text-sub">PRODUCT</p>
+        </a>
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >お知らせ
+          <p class="nav-text-sub">NEWS</p>
+        </a>
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >出品者募集
+          <p class="nav-text-sub">SHIPPERS</p>
+        </a>
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >お問い合わせ
+          <p class="nav-text-sub">CONTACT US</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+}
+.nav {
+  position: relative;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  justify-content: center;
+  padding: 8%;
+  background: var(--background-color);
+  overflow: hidden;
+  &-text {
+    z-index: 2;
+    font-weight: 600;
+    font-size: 1rem;
+    text-align: center;
+    letter-spacing: 0.06rem;
+    &-sub {
+      font-family: var(--josefin-sans);
+      font-size: 0.8rem;
+      font-weight: 600;
+      color: #5f8934;
+      letter-spacing: 0.08rem;
+      margin-top: 0.3rem;
+    }
+  }
+  .shape-upper-left {
+    position: absolute;
+    top: -10%;
+    left: -10%;
+    width: 300px;
+    height: 300px;
+    border-radius: 50vh;
+    background: #f4eed8;
+  }
+  .shape-bottom-right {
+    position: absolute;
+    bottom: -20%;
+    right: -10%;
+    width: 300px;
+    height: 300px;
+    border-radius: 50vh;
+    background: #f4eed8;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,209 @@
+<script setup lang="ts">
+import DrawerMenu from "./DrawerMenu.vue";
+import { computed, ref } from "vue";
+
+const isOpen = ref(false);
+const openMenu = () => {
+  isOpen.value = !isOpen.value;
+};
+const openClass = computed(() => {
+  return { "-open": isOpen.value };
+});
+</script>
+
+<template>
+  <header :class="openClass">
+    <div class="logo">
+      <div class="logo-icon">
+        <a href="#"><img src="/svgs/logo.svg" alt="logo" /></a>
+      </div>
+      <div>
+        <p class="logo-text">さくらいとれとれ市場</p>
+        <p class="logo-text-sub">Sakurai Toretore Market</p>
+      </div>
+    </div>
+    <div class="icons">
+      <div class="shopping-cart">
+        <a href="#">
+          <img class="shopping-cart-img" src="/svgs/shopping-cart-black.svg" alt="shoppng-cart" />
+        </a>
+      </div>
+      <div class="hamburger-menu" @click.prevent="openMenu">
+        <div class="line-group">
+          <span class="line"></span>
+          <span class="line"></span>
+          <span class="line"></span>
+        </div>
+        <p>MENU</p>
+      </div>
+    </div>
+
+    <ul class="nav">
+      <li class="nav-text">
+        <a href="#"
+          >店舗情報
+          <p class="nav-text-sub">INFOMATION</p></a
+        >
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >取り扱い商品
+          <p class="nav-text-sub">PRODUCT</p></a
+        >
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >お知らせ
+          <p class="nav-text-sub">NEWS</p></a
+        >
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >出品者募集
+          <p class="nav-text-sub">SHIPPERS</p></a
+        >
+      </li>
+      <li class="nav-text">
+        <a href="#"
+          >お問い合わせ
+          <p class="nav-text-sub">CONTACT US</p></a
+        >
+      </li>
+    </ul>
+  </header>
+  <Transition name="fade">
+    <div v-if="isOpen">
+      <DrawerMenu />
+    </div>
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  width: 100vw;
+  padding: 1% 3% 1% 3%;
+  z-index: 100;
+  &.-open {
+    animation: fadeIn 0.6s ease-out forwards;
+  }
+
+  @keyframes fadeIn {
+    20% {
+      background: #fff;
+    }
+    100% {
+      background: none;
+    }
+  }
+}
+
+.logo {
+  display: flex;
+  align-items: flex-end;
+}
+.logo-icon {
+  width: 4rem;
+  height: 4rem;
+}
+.logo-text {
+  font-family: var(--m-plus-rounded-1c);
+  font-size: 1.3rem;
+  font-weight: 700;
+}
+.logo-text-sub {
+  font-size: 0.9rem;
+  color: #5f8934;
+  font-family: var(--josefin-sans);
+  letter-spacing: 0.03rem;
+}
+
+.nav {
+  display: flex;
+  gap: 3rem;
+}
+.nav-text {
+  font-weight: 500;
+  font-size: 1rem;
+  text-align: center;
+  letter-spacing: 0.06rem;
+}
+.nav-text-sub {
+  font-family: var(--josefin-sans);
+  font-size: 0.6rem;
+  font-weight: 400;
+  color: #5f8934;
+  letter-spacing: 0.08rem;
+}
+
+.shopping-cart {
+  display: none;
+}
+
+.hamburger-menu {
+  display: none;
+}
+
+@media screen and (max-width: 767px) {
+  header {
+    align-items: flex-end;
+  }
+  .logo-text {
+    font-size: 15px;
+  }
+  .logo-text-sub {
+    font-size: 9px;
+  }
+  .nav {
+    display: none;
+  }
+  .icons {
+    display: flex;
+    gap: 1rem;
+  }
+  .shopping-cart {
+    display: flex;
+    border-radius: 50%;
+    border: solid 1.3px;
+    width: 2.6rem;
+    height: 2.6rem;
+    justify-content: center;
+    align-items: center;
+  }
+  .shopping-cart-img {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+  .hamburger-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
+    gap: 5px;
+  }
+
+  .hamburger-menu .line-group {
+    display: flex;
+    flex-direction: column;
+    gap: 0.6rem;
+  }
+
+  .hamburger-menu .line {
+    width: 35px;
+    height: 1.3px;
+    background-color: black;
+  }
+
+  .hamburger-menu p {
+    font-size: 0.8rem;
+    font-family: var(--josefin-sans);
+    font-weight: 500;
+  }
+}
+</style>
